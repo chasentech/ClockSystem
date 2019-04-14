5025509c5be10065e3120eed8c84b644273f3b37
zcl <xiyezcl@foxmail.com>
2019-04-14 13:29:28 +0800

    delete some

--- Devices/bell.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BELL_H_
-#define _BELL_H_
-
-extern void bell_init(void);
-extern void bell_short_ring(unsigned int times);
-extern void bell_alarm_ring(void);
-extern void bell_alarm_close(void);
-
-#endif
